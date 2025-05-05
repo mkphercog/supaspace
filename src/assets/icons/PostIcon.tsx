c3e20672f508fcba76c18de415900e6b53562866
Marcin Hercog <mkp.hercog@gmail.com>
2025-05-05 12:49:38 +0200

    feat: change PostIcon

--- src/assets/icons/PostIcon.tsx
+++ src/assets/icons/PostIcon.tsx
@@ -1,14 +1,18 @@
-export const PostIcon = ({ className }: { className?: string }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
-    width="24"
-    height="24"
-    className={`shrink-0 ${className}`}
-  >
-    <path
-      fill="currentColor"
-      d="M19 21H5q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h14q.825 0 1.413.588T21 5v14q0 .825-.587 1.413T19 21M6 14h12v-2H6zm0 3h12v-1.5H6z"
-    />
-  </svg>
-);
+import type { SVGProps } from "react";
+
+export function PostIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 20 20"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="m16.89 1.2l1.41 1.41c.39.39.39 1.02 0 1.41L14 8.33V18H3V3h10.67l1.8-1.8c.4-.39 1.03-.4 1.42 0m-5.66 8.48l5.37-5.36l-1.42-1.42l-5.36 5.37l-.71 2.12z"
+      ></path>
+    </svg>
+  );
+}
